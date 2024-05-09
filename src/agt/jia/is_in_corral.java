0696324c59aff0efa4e857cb39e6b9063a6bb313
Martin <cdevroot@gmail.com>
2024-05-09 22:45:23 +0700

    Fix packages.

--- src/agt/jia/is_in_corral.java
+++ src/agt/jia/is_in_corral.java
@@ -6,18 +6,14 @@ import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
 import jason.asSyntax.NumberTerm;
-import jason.asSyntax.NumberTermImpl;
 import jason.asSyntax.Term;
 import jason.environment.grid.Location;
-import jason.environment.grid.Area;
-
-import jia.get_corral_area;
 
 public class is_in_corral extends DefaultInternalAction {
-    
+
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-        try{
+        try {
             int locX = (int) ((NumberTerm) args[0]).solve();
             int locY = (int) ((NumberTerm) args[1]).solve();
             var loc = new Location(locX, locY);
@@ -29,7 +25,7 @@ public class is_in_corral extends DefaultInternalAction {
             System.out.println("Test");
 
             return corral.contains(loc);
-        } catch(NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             return false;
         }
     }
