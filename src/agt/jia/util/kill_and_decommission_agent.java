d9c896c31349b4d1b70f28d081791c8b684457f5
Martin <cdevroot@gmail.com>
2024-06-05 19:41:22 +0700

    Minor fix.

--- src/agt/jia/util/kill_and_decommission_agent.java
+++ src/agt/jia/util/kill_and_decommission_agent.java
@@ -7,15 +7,17 @@ import jason.asSyntax.StringTermImpl;
 import jason.asSyntax.Term;
 import jason.stdlib.kill_agent;
 import model.AgentInfo;
+import service.AgentDB;
 
 public class kill_and_decommission_agent extends kill_agent {
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         AgentInfo agent = AgentUtil.getAgentInfoFromTs(ts);
+        String agentName = agent.getJasonId();
 
         GridModel.getInstance().removeAgent(agent);
+        AgentDB.getInstance().removeAgent(agent.getCartagoId());
 
-        String agentName = agent.getJasonId();
         return super.execute(ts, un, new Term[] { new StringTermImpl(agentName) });
     }
 }
