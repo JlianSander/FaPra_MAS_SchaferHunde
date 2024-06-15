c91666f7838620bfa03c1759b5e1376d28e1f9a3
JlianSander <86343096+JlianSander@users.noreply.github.com>
2024-06-15 16:50:24 +0200

    made get_next_pos iterativ instead of recursive

--- src/agt/jia/get_next_pos.java
+++ src/agt/jia/get_next_pos.java
@@ -24,7 +24,7 @@ public class get_next_pos extends DefaultInternalAction {
         //ts.getLogger().info("--------------'get_next_pos' ");                                                                     //DEBUG
         GridModel model = GridModel.getInstance();
         PropertiesLoader loader = PropertiesLoader.getInstance();
-        Integer maxNumberCalls = loader.getProperty("hound_max_calls_new_target_pos", Integer.class);
+        Integer maxNumberRecalculations = loader.getProperty("hound_max_recalc_new_target_pos", Integer.class);
         Integer keepDistanceToSheep = loader.getProperty("hound_keep_distance_to_sheep", Integer.class);
 
         int myX = (int) ((NumberTerm) args[0]).solve();
@@ -35,7 +35,7 @@ public class get_next_pos extends DefaultInternalAction {
         int targetY = (int) ((NumberTerm) args[3]).solve();
         var targetLoc = new Location(targetX, targetY);
 
-        Location validTarget = calculateValidTargetRecursiv(model, maxNumberCalls, keepDistanceToSheep, myLoc, targetLoc, 0);
+        Location validTarget = calculateValidTarget(model, maxNumberRecalculations, keepDistanceToSheep, myLoc, targetLoc);
         //ts.getLogger().info("--------------'get_next_pos' next Line: Pathfinder.getInstance");                                                                        //DEBUG
         Pathfinder pathfinder = Pathfinder.getInstance(GridModel.HOUND);
         //ts.getLogger().info("--------------'get_next_pos' next Line: pathfinder.excludeObjects");                                                                     //DEBUG
@@ -46,78 +46,86 @@ public class get_next_pos extends DefaultInternalAction {
                     && un.unifies(args[5], new NumberTermImpl(nextPos.y));
     }
 
-    private Location calculateValidTargetRecursiv(GridModel model, Integer maxNumberCalls, Integer keepDistanceToSheep, Location myLoc, Location desiredTarget, int numberCalls){
-        //TERMINATION condition
-        if(desiredTarget.equals(myLoc) || !model.inGrid(desiredTarget) || numberCalls > maxNumberCalls){
-            // return start location, so that agent does not move
-            return myLoc;
-        }
-
-        var sheepNearBy = model.getNeighborhood(desiredTarget, keepDistanceToSheep, loc -> {
-            List<Integer> objects = model.getObjectsAt(loc);
-            return objects.contains(GridModel.SHEEP) ;
-        });
+    private Location calculateValidTarget(GridModel model, Integer maxNumberRecalculations, Integer keepDistanceToSheep, Location myLoc, Location originalTarget){
         
-        if(model.isFree(desiredTarget) && sheepNearBy.isEmpty()){
-            return desiredTarget;
-        }else{
-            //target position is not valid and needs to change
-            //calculate a new target position, which is supposedly valid
-            Location calculatedNewTarget;
+        Queue<Location> locsToProcess = new ArrayDeque<Location>();
+        locsToProcess.add(originalTarget);
+        int numberRecalculations = 0;
+
+        while(!locsToProcess.isEmpty()){
+            Location targetToProcess = locsToProcess.poll();
             
-            if(!model.isFree(desiredTarget)){
-                //target position itself is not valid    
-                //calculate direction towards the target
-                RealVector directionToTarget = MatrixUtils.createRealVector(new double[] {
-                    desiredTarget.x - myLoc.x,
-                    desiredTarget.y - myLoc.y
-                });
-    
-                if( directionToTarget.getEntry(0) != 0 || directionToTarget.getEntry(1) != 0){
-                    directionToTarget = directionToTarget.unitVector();
-                }
-                //invert direction
-                RealVector directionAwayFromTarget =  directionToTarget.mapMultiply(-1);
-    
-                //calculate new target position
-                calculatedNewTarget = new Location(
-                    desiredTarget.x
-                        + (int) Math.round(
-                            directionAwayFromTarget.getEntry(0) * 1),
-                    desiredTarget.y 
-                        + (int) Math.round(
-                            directionAwayFromTarget.getEntry(1) * 1));
-    
+            var sheepTooCloseBy = model.getNeighborhood(targetToProcess, keepDistanceToSheep, loc -> {
+                List<Integer> objects = model.getObjectsAt(loc);
+                return objects.contains(GridModel.SHEEP) ;
+            });
+            
+            if(model.isFree(targetToProcess) && sheepTooCloseBy.isEmpty()){
+                return targetToProcess;
             }else{
-                //sheeps are too close to target position
-                // focus on first sheep in list
-                Location locSheep = sheepNearBy.get(0); 
+                //target position is not valid and needs to change
+                //calculate a new target position, which is supposely valid
+                Location calculatedNewTarget;
+                
+                if(!model.isFree(targetToProcess)){
+                    //target position itself is not valid    
+                    //calculate direction towards the target
+                    RealVector directionToTarget = MatrixUtils.createRealVector(new double[] {
+                        targetToProcess.x - myLoc.x,
+                        targetToProcess.y - myLoc.y
+                    });
+        
+                    if( directionToTarget.getEntry(0) != 0 || directionToTarget.getEntry(1) != 0){
+                        directionToTarget = directionToTarget.unitVector();
+                    }
+                    //invert direction
+                    RealVector directionAwayFromTarget =  directionToTarget.mapMultiply(-1);
+        
+                    //calculate new target position
+                    calculatedNewTarget = new Location(
+                        targetToProcess.x
+                            + (int) Math.round(
+                                directionAwayFromTarget.getEntry(0) * 1),
+                        targetToProcess.y 
+                            + (int) Math.round(
+                                directionAwayFromTarget.getEntry(1) * 1));
+                }else{
+                    //sheeps are too close to target position
+                    // focus on first sheep in list
+                    Location locSheep = sheepTooCloseBy.get(0); 
 
-                //calculate direction towards the first sheep
-                RealVector directionToSheep = MatrixUtils.createRealVector(new double[] {
-                    locSheep.x - desiredTarget.x,
-                    locSheep.y - desiredTarget.y
-                });
+                    //calculate direction towards the first sheep
+                    RealVector directionToSheep = MatrixUtils.createRealVector(new double[] {
+                        locSheep.x - targetToProcess.x,
+                        locSheep.y - targetToProcess.y
+                    });
 
-                if( directionToSheep.getEntry(0) != 0 || directionToSheep.getEntry(1) != 0){
-                    directionToSheep = directionToSheep.unitVector();
+                    if( directionToSheep.getEntry(0) != 0 || directionToSheep.getEntry(1) != 0){
+                        directionToSheep = directionToSheep.unitVector();
+                    }
+                    //invert direction
+                    RealVector directionAwayFromSheep =  directionToSheep.mapMultiply(-1);
+
+                    //calculate new target position
+                    calculatedNewTarget = new Location(
+                        targetToProcess.x
+                            + (int) Math.round(
+                                directionAwayFromSheep.getEntry(0) * 1),
+                        targetToProcess.y 
+                            + (int) Math.round(
+                                directionAwayFromSheep.getEntry(1) * 1));
                 }
-                //invert direction
-                RealVector directionAwayFromSheep =  directionToSheep.mapMultiply(-1);
 
-                //calculate new target position
-                calculatedNewTarget = new Location(
-                    desiredTarget.x
-                        + (int) Math.round(
-                            directionAwayFromSheep.getEntry(0) * 1),
-                    desiredTarget.y 
-                        + (int) Math.round(
-                            directionAwayFromSheep.getEntry(1) * 1));
+                //avoid StackOverflow
+                numberRecalculations++;
+                if(numberRecalculations > maxNumberRecalculations){
+                    return myLoc;
+                }
+
+                locsToProcess.add(calculatedNewTarget);
             }
-
-            //RECURSIVE CALL
-            //call method again to check new target position
-            return calculateValidTargetRecursiv(model, maxNumberCalls, keepDistanceToSheep, myLoc, calculatedNewTarget, numberCalls++);
-        }
+        }      
+        
+        return myLoc;
     }
 }
