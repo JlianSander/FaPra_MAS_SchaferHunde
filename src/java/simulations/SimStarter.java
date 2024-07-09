6357eba434739e497591d8f5d6b91c240e3661d3
Martin <cdevroot@gmail.com>
2024-07-09 21:12:34 +0700

    Add times param + output strategy in excel file.

--- src/java/simulations/SimStarter.java
+++ src/java/simulations/SimStarter.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import jacamo.infra.JaCaMoLauncher;
 import jason.JasonException;
 import util.FileLister;
@@ -27,7 +30,17 @@ public class SimStarter {
 
         controlFrame.getContentPane().add(stopButton);
         controlFrame.setSize(200, 100);
+
+        stopButton.setBackground(java.awt.Color.RED);
+        stopButton.setForeground(java.awt.Color.WHITE);
+
         controlFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = screenSize.width - controlFrame.getWidth();
+        int y = screenSize.height - controlFrame.getHeight();
+        controlFrame.setLocation(x, y);
+
         controlFrame.setVisible(true);
     }
 
@@ -44,52 +57,80 @@ public class SimStarter {
     public static void main(String[] args) throws JasonException, IOException {
         createControlWindow();
 
+        int times = 0;
+        try {
+            times = Integer.parseInt(System.getProperty("times"));
+        } catch (NumberFormatException e) {
+            logger.severe("Invalid number of times to run the simulation");
+            System.exit(1);
+        }
+
+        boolean full = Boolean.parseBoolean(System.getProperty("full"));
+
         String dir = "simulations/scenarios";
         List<String> scenarios = FileLister.getFileNames(dir, false);
 
         PropertiesLoader loader = PropertiesLoader.getInstance();
 
-        int houndStrategyDriveClusterSwarm = loader.getProperty("hound_strategy_cluster_swarm_amount", Integer.class);
-        int houndStrategyDriveSelectSwarm = loader.getProperty("hound_strategy_select_swarm_amount", Integer.class);
-        int houndStrategyDriveDrive = loader.getProperty("hound_strategy_drive_amount", Integer.class);
+        int i = full ? 1 : loader.getProperty("hound_strategy_cluster_swarm", Integer.class);
+        int j = full ? 1 : loader.getProperty("hound_strategy_select_swarm", Integer.class);
+        int k = full ? 1 : loader.getProperty("hound_strategy_drive", Integer.class);
 
-        for (int i = 1; i <= houndStrategyDriveClusterSwarm; i++) {
-            for (int j = 1; j <= houndStrategyDriveSelectSwarm; j++) {
-                for (int k = 1; k <= houndStrategyDriveDrive; k++) {
-                    logger.info("+++ Starting simulation +++");
-                    logger.info("Hound DRIVE strategy:");
-                    logger.info("ClusterSwarm: " + i);
-                    logger.info("SelectSwarm: " + j);
-                    logger.info("Drive: " + k);
+        int houndStrategyDriveClusterSwarmAmount = full
+                ? loader.getProperty("hound_strategy_cluster_swarm_amount", Integer.class)
+                : i;
+        int houndStrategyDriveSelectSwarmAmount = full
+                ? loader.getProperty("hound_strategy_select_swarm_amount", Integer.class)
+                : j;
+        int houndStrategyDriveDriveAmount = full ? loader.getProperty("hound_strategy_drive_amount", Integer.class) : k;
 
-                    for (String scenario : scenarios) {
+        System.out.println("=== Starting simulation a total of " + times + " times ===\n");
+        if (full) {
+            System.out.println("+++ FULL mode: Iterating through all strategies +++\n\n");
+        }
 
-                        // Spawn the simulation in a separate JVM process
-                        ProcessBuilder processBuilder = new ProcessBuilder(
-                                "java",
-                                "-Xms16g",
-                                "-Xmx16g",
-                                "-XX:+UseG1GC",
-                                "-XX:MaxGCPauseMillis=200",
-                                "-DsimName=" + scenario,
-                                "-DhoundStrategyClusterSwarm=" + i,
-                                "-DhoundStrategySelectSwarm=" + j,
-                                "-DhoundStrategyDrive=" + k,
-                                "-cp",
-                                System.getProperty("java.class.path"),
-                                JaCaMoLauncher.class.getName(),
-                                dir + "/" + scenario);
+        for (int t = 0; t < times; t++) {
+            while (i <= houndStrategyDriveClusterSwarmAmount) {
+                while (j <= houndStrategyDriveSelectSwarmAmount) {
+                    while (k <= houndStrategyDriveDriveAmount) {
+                        logger.info("+++ Starting simulation +++");
+                        logger.info("Hound DRIVE strategy:");
+                        logger.info("ClusterSwarm: " + i);
+                        logger.info("SelectSwarm: " + j);
+                        logger.info("Drive: " + k);
 
-                        try {
-                            simulationProcess = processBuilder.start();
+                        for (String scenario : scenarios) {
 
-                            // Wait for the sim to complete
-                            simulationProcess.waitFor();
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                            // Spawn the simulation in a separate JVM process
+                            ProcessBuilder processBuilder = new ProcessBuilder(
+                                    "java",
+                                    "-Xms16g",
+                                    "-Xmx16g",
+                                    "-XX:+UseG1GC",
+                                    "-XX:MaxGCPauseMillis=200",
+                                    "-DsimName=" + scenario,
+                                    "-DhoundStrategyClusterSwarm=" + i,
+                                    "-DhoundStrategySelectSwarm=" + j,
+                                    "-DhoundStrategyDrive=" + k,
+                                    "-cp",
+                                    System.getProperty("java.class.path"),
+                                    JaCaMoLauncher.class.getName(),
+                                    dir + "/" + scenario);
+
+                            try {
+                                simulationProcess = processBuilder.start();
+
+                                // Wait for the sim to complete
+                                simulationProcess.waitFor();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
+                        k++;
                     }
+                    j++;
                 }
+                i++;
             }
         }
     }
